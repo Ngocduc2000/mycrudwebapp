9fc582942fffad5cb8682a4107b3be867f4966f2
ADMIN\canng <mcer.slifer@gmail.com>
2023-02-25 14:52:46 +0700

    Initial commit

---
+++ src/main/java/com/example/crudwebapp/Service/Impl/UserServiceImpl.java
@@ -0,0 +1,38 @@
+package com.example.crudwebapp.Service.Impl;
+
+import com.example.crudwebapp.Service.UserService;
+import com.example.crudwebapp.entity.User;
+import com.example.crudwebapp.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<User> getAllUser() {
+        return (List<User>) userRepository.findAll();
+    }
+
+    @Override
+    public void saveUser(User user) {
+        userRepository.save(user);
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    @Override
+    public Optional<User> findUserById(Long id) {
+        return userRepository.findById(id);
+    }
+}
---
+++ src/main/java/com/example/crudwebapp/entity/User.java
@@ -0,0 +1,35 @@
+package com.example.crudwebapp.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Email;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+
+    @Column(name = "phone")
+    private String phone;
+
+}
